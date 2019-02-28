39d90e7a12f91e207de46a2b8e52fe39828358d2
ABDURRAHMANSER <34307753+ABDURRAHMANSER@users.noreply.github.com>
2019-02-28 14:02:04 +0300

    Add files via upload

---
+++ pycharm.cmd
@@ -0,0 +1,16 @@
+@echo off 
+SET OSGEO4W_ROOT=C:\OSGeo4W64
+call "%OSGEO4W_ROOT%"\bin\o4w_env.bat
+
+@echo off 
+path %PATH%;%OSGEO4W_ROOT%\apps\qgis\bin
+path %PATH%;%OSGEO4W_ROOT%\apps\grass\grass-7.4.0\lib
+path %PATH%;C:\OSGeo4W64\apps\QT5\bin
+path %PATH%;C:\OSGeo4W64\apps\Python37\Scripts
+
+set PYTHONPATH=%PYTHONPATH%;%OSGEO4W_ROOT%\apps\qgis\python
+set PYTHONHOME=%OSGEO4W_ROOT%\apps\Python37
+
+set PATH=C:\Program files\Git\bin;%PATH%
+
+start "PyCharm aware of QGIS" /B "C:\Program Files (x86)\JetBrains\PyCharm 2016.3.3\bin\pycharm.exe"
